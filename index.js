68b3ce8c7120fe53cc4842f03873b27caed136ef
franklin anyaogu <fanyaogu1@byupathway.edu>
2026-07-01 02:13:51 +0100

    first check of my repo

---
+++ index.js
@@ -0,0 +1,21 @@
+const root = document.getElementById('root');
+
+root.innerHTML = `
+     <header>
+        <h1>This is a new one</h1>
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, sed iste. Accusantium ullam ipsam eveniet delectus et, sapiente nulla minus cumque modi quo sint possimus maiores, obcaecati reiciendis quasi aperiam ut. Commodi asperiores ullam incidunt magnam sequi recusandae, sed quam aliquam, possimus non corporis, tempora cum quia dolorum excepturi libero!</p>
+
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident aperiam tempore neque repellat blanditiis, esse obcaecati tempora consequatur laborum error dolore accusantium fugiat natus numquam sequi odit animi doloremque soluta nostrum repudiandae quasi sunt placeat ea? Qui labore magni non asperiores sunt neque sapiente modi rerum natus unde impedit ipsum at quasi, quaerat sequi ad. Accusantium voluptate alias voluptatem.</p>
+    </main>
+
+    <footer>&copy; 2026 - Anyaogu Franklin</footer>
+`;
